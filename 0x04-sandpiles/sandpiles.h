3555a0cbc141d18dd0c69af88d74691fdbb99658
jashjchoi <jashjchoi@gmail.com>
2021-08-20 05:00:49 +0000

    update header

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void stable_grid(int grid1[3][3], int grid2[3][3]);
-int valid(int grid[3][3]);
+int valid_grid(int grid[3][3]);
 
 #endif
