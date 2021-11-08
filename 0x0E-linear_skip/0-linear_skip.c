283f21ae3cae3b58f7401ee9a1c60d9dead00be2
jashjchoi <jashjchoi@gmail.com>
2021-11-08 04:02:53 +0000

    set up the project folder for interview q

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,46 @@
+#include "search.h"
+/**
+*linear_skip - function to perform a search in a linear skip
+*@list: Head of the list of type skiplis_t
+*@value: int value to search in the list
+*Return: node contain to value, NULL otherwise
+*/
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *tmp;
+
+	if (!list)
+		return (NULL);
+	tmp = list;
+
+	while (tmp)
+	{
+		printf("Value checked at index [%ld] = [%d]\n",
+			tmp->index, tmp->n);
+		if (tmp != NULL && tmp->express != NULL &&
+			tmp->express->n >= value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+				tmp->index, tmp->express->index);
+			break;
+		}
+
+		if (tmp->express->express == NULL && tmp->express->n < value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+				tmp->index, tmp->express->index);
+			break;
+		}
+		tmp = tmp->express;
+	}
+	while (tmp)
+	{
+		printf("Value checked at index [%ld] = [%d]\n",
+			tmp->index, tmp->n);
+		if (tmp->n == value)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
