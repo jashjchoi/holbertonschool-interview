d2740659a651c5e4fcd807c40f414cf67fe81c00
jashjchoi <jashjchoi@gmail.com>
2022-01-21 05:00:10 +0000

    Fixed whitespace errors

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -42,12 +42,11 @@ void merge_sub(int *subarr, int *array, size_t left, size_t mid, size_t right)
 		else
 			subarr[k++] = array[j++];
 	}
-
-    while (j < right)
-        subarr[k++] = array[j++];
-
 	while (i < mid)
-        subarr[k++] = array[i++];
+		subarr[k++] = array[i++];
+
+	while (j < right)
+		subarr[k++] = array[j++];
 
 	for (k = left, i = 0; k < right; k++)
 		array[k] = subarr[i++];
