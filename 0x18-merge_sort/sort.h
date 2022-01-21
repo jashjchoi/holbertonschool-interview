dd23c0fd9ff871694a2007b7ce349adaabc98f93
jashjchoi <jashjchoi@gmail.com>
2022-01-21 05:02:07 +0000

    debugging errors

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -8,4 +8,5 @@ void print_array(const int *array, size_t size);
 void merge_sub(int *subarr, int *array, size_t left, size_t mid, size_t right);
 void split_arr(int *subarr, int *array, size_t start, size_t end);
 void merge_sort(int *array, size_t size);
+
 #endif
